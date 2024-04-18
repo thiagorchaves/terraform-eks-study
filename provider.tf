eb0ff24bb07a475c95168c72e388732ce0aa0717
Thiago Chaves <thiago.chaves@enjoei.com.br>
2024-04-18 18:28:08 -0300

    criando vpc eks

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.45.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.29.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-study-comunidadedevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    profile = "terraform-study"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform-study"
+}
